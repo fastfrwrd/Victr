6afcffc13351f9e5d61bbcb7af6669f5cadeb0cf
Your Name <fastfrwrd@gmail.com>
2012-02-16 10:13:12 -0600

    first commit

---
+++ views.py
@@ -0,0 +1,8 @@
+# Create your views here.
+from django.http import HttpResponse
+import datetime
+
+def index(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
