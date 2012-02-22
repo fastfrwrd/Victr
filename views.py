c36adeccc880132bb1700a321bee4cf0c860f616
Raul Mireles <raul@Admins-MacBook-Pro.local>
2012-02-22 12:25:24 -0600

    After conflict: the pushing the same views function

--- views.py
+++ views.py
@@ -1,18 +1,29 @@
 from django.http import HttpResponse
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
 from django.utils import simplejson as json
 from django.core import serializers
 from django.core.urlresolvers import reverse
-from victr.models import *
 from django.contrib.auth.decorators import login_required
+from django.contrib import auth
+from victr.models import *
+from victr.forms import RegistrationForm
 
-@login_required
-def vip(request):
-    """
-    temporary placement of hypothetical page with restricted access
-    """
-    return HttpResponse("Secret Magical Page", {})
+def register(request):
+    registration_form = RegistrationForm()
+    if request.method == 'POST' :
+        registration_form = RegistrationForm(request.POST)
+        if registration_form.is_valid() :
+            try :
+                registration_form.save()    #user registered
+            except : #IntegrityError as detail :
+                print "username already registered we need to handle that gracefully"
+                return render_to_response("register.html", locals(), context_instance=RequestContext(request))
+            current_user = auth.authenticate(username=registration_form.cleaned_data['email'],
+                                             password=registration_form.cleaned_data['password'])
+            auth.login(request, current_user)
+            return redirect('/vip')
+    return render_to_response("register.html", locals(), context_instance=RequestContext(request))
 
 def login(request):
     """
@@ -21,3 +32,14 @@ def login(request):
     ?next get variable handling can also go here
     """
     return HttpResponse("Login Please", {})
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponse("Logged out.", {})
+
+@login_required
+def vip(request):
+    """
+    temporary placement of hypothetical page with restricted access
+    """
+    return HttpResponse("Secret Magical Page", {})
