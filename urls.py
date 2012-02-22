54d4ab5d2112f0b22de0cc8dadbacad15511b6a3
Paul Marbach <fastfrwrd@gmail.com>
2012-02-21 22:05:45 -0600

    urls cleanup

--- urls.py
+++ urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-	url(r'^$', include('victr.event.urls'), name='current'),
+	url(r'^$', include('victr.event.urls')),
     url(r'^api', include('victr.api.urls')),
     url(r'^project', include('victr.project.urls')),
-    url(r'^impress/', include('victr.impress.urls')),
-    (r'login/$', 'victr.views.login')                        
+    url(r'^impress', include('victr.impress.urls')),
+    url(r'^login/$', 'victr.views.login')                        
 )
