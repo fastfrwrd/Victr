c9e60bcd3e001eec86094a98e614a281641e6d1c
Raul Mireles <raul@Admins-MacBook-Pro.local>
2012-02-22 12:28:58 -0600

    After conflict: copied url patterns into victr/urls.py for fixin'

--- urls.py
+++ urls.py
@@ -5,5 +5,10 @@ urlpatterns = patterns('',
     url(r'^api', include('victr.api.urls')),
     url(r'^project', include('victr.project.urls')),
     url(r'^impress', include('victr.impress.urls')),
-    url(r'^login/$', 'victr.views.login')                        
+    url(r'^login/$', 'victr.views.login'),
+    #temporary: below are the patterns as they appear in main/urls.py for the auth work
+    (r'register/$', 'victr.views.register'), #temporary login work
+    (r'login/$', 'victr.views.login'), #temporary login work
+    (r'vip/$', 'victr.views.vip'), #temporary login work
+    (r'logout/$', 'victr.views.logout'), #temporary login work
 )
