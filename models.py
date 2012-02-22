d7ffa3425f30fb373b1a02509946df5ab70439a6
Shela Xu <shela.xu@gmail.com>
2012-02-22 10:31:00 -0600

    models.py

--- models.py
+++ models.py
@@ -19,7 +19,6 @@ class Project(models.Model):
     description = models.TextField(blank=True)
     mainUrl = models.URLField()
     collaborators = models.ManyToManyField(User, blank=True)
-    screenshot = models.ImageField(upload_to="images", blank=True) #this should be images/<event_id>/screenshots
     tech = models.ManyToManyField(Disciplines, blank=True)
     def save(self):
         if not self.id:
